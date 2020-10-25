a210fd47a40f757f8c5817a8c2103ff465edf82c
Alexander <alex.pismenskiy@gmail.com>
2020-10-25 22:20:29 +0300

    Alert for name created

--- PersonalInfoApp/ViewController.swift
+++ PersonalInfoApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+    
+    @IBAction func userNameReminderButton() {
+        showAlertName(with: "Your User Name is", and: "Alex")
+    }
+    
+    @IBAction func passwordReminderButton() {
+    }
 }
 
+
+extension ViewController {
+    private func showAlertName(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userNameTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
